9b7f80de8fd9b0d11b67dc9bcafbd184ab22283d
Juhász Zoltán <juhaszzoltanmate@yahoo.com>
2023-03-16 09:40:49 +0100

    Add project files.

---
+++ MTest.cs
@@ -0,0 +1,61 @@
+﻿namespace CA230316
+{
+    internal static class MTest
+    {
+        internal static List<Person> GetTestList()
+            => new()
+            {
+                new()
+                {
+                    Id = 1,
+                    Name = "Nicole Mahmoud",
+                    BirthDate = new(1987, 03, 17),
+                    Hobbies = new string[] { "origami", "painting", "horses", },
+                    Sex = false,
+                },
+                new()
+                {
+                    Id = 2,
+                    Name = "Alexandre Jeong-Suk",
+                    BirthDate = new(2004, 04, 19),
+                    Hobbies = new string[] { "music", "computer games", },
+                    Sex = true,
+                },
+                new()
+                {
+                    Id = 3,
+                    Name = "Violeta Jehohanan",
+                    BirthDate = new(1997, 02, 11),
+                    Hobbies = new string[] { "fishing", "alcohol", },
+                    Sex = false,
+                },
+                new()
+                {
+                    Id = 4,
+                    Name = "Gherardo Olwyn",
+                    BirthDate = new(2013, 05, 23),
+                    Hobbies = new string[] { "fishing", "computer games", "horses", },
+                    Sex = true,
+                },
+                new()
+                {
+                    Id = 5,
+                    Name = "Jayendra Tzeruyah",
+                    BirthDate = new(1935, 12, 02),
+                    Hobbies = new string[] { "take pills", "sleeping", "cooking", },
+                    Sex = false,
+                },
+            };
+    }
+
+    internal class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int Age => DateTime.Today.Year - BirthDate.Year;
+        public string[] Hobbies { get; set; }
+        public bool Sex { get; set; }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using CA230316;
+
+List<Person> people = MTest.GetTestList();
+
+//sorozatszámítás (összegzés)
+int noh = people.Sum(p => p.Hobbies.Length);
+Console.WriteLine($"hobbik száma a listában: {noh} db");
+
+//-> átlag
+double avg = people.Average(p => p.Age);
+Console.WriteLine($"átlagéletkor: {avg}");
+
+//megszámlálás
+int non = people.Count(p => p.Age >= 18);
+Console.WriteLine($"nagykorúak száma: {non}");
+
+//szélsőérték hely/index/példány
+Person lgf = people.MinBy(p => p.Age)!;
+Console.WriteLine($"legfiatalabb neve: {lgf.Name}");
+
+//szélsőérték
+int lgk = people.Max(p => p.Age);
+Console.WriteLine($"legnagyobb kor: {lgk}");
+
+//eldöntés
+bool vekn = people.Any(p => p.BirthDate.Year == 2004);
+Console.WriteLine($"{(vekn ? "van" : "nincs")} olyan, aki 2004ben született");
+
+//kiv./linker
+
+// -> mindnél a return value: object
+
+//firstOrDef [EZ a LINKER]
+// -> HA keresési feltétel legalább egy elemre IGAZ:
+//      -> visszaadja a lekisbb indexű, keresésnek megfelelő objektumot
+// -> EGYÉBKÉNT
+//      -> HA a keresebb obj. VALUE TYPE:
+//          -> visszaadja az struct példány ZERO értékét
+//      -> HA a keresett obj. REF TYPE:
+//          -> null értéket ad vissza
+
+Person alex = people.FirstOrDefault(p => p.Name.ToLower().StartsWith("alex"))!;
+Console.WriteLine(alex is null 
+    ? "NINCS olyan, akineke a neve alex-el kezdődne" 
+    : $"{alex.Name} neve alex-el kezdődik");
+
+//lineáris keresés -> index (példány)
+//kiválasztás
+
+//first
+// abban különbözik, hogy ha nincs találat a keresésre,
+// akkor nem null vagy zero a return, hanem exception
+
+//last && lastOrDef
+// találat esetén a legNAGYOBB indexű obj.-t adja vissza
+
+//single
+// -> ha a keresési feltétel PONTOSAN egy elemre igaz, akkor visszadja azt az elemet
+// -> ha többre is igaz, akkor exception
+// -> ha nincs találat, akkor is exception (csak más)
+//singleOrDef
+// -> ha pont 1 találat van akkor ua. mint a single
+// -> ha többre is igaz, akkor is ua. mint single (tehát kivételt dob)
+// -> ha egyre sem igaz, akkor vagy null vagy zero
+
+
+//kiválogatás
